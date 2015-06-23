844b07c696bdaeeba1e3a0bcffbc18afa4215b43
Le Hong <le_hong@mentor.com>
2015-06-23 14:00:11 -0700

    Add unique insertion and while loop

--- qifei.py
+++ qifei.py
@@ -17,6 +17,7 @@ This function is designed to analyze general public feeling toward certain stock
 """ --- Parsing cmdLine options --- """
 parser = optparse.OptionParser()
 parser.add_option('-i', '--in', dest='sname',  default='MENT',                                  help='Stock symble.')
+parser.add_option('-t',         dest='t',      default=60,    action='store',      type='int',  help='Time span in secs')
 # parser.add_option('-l',         dest='doLive', default=False, action='storr_true',              help='live chart for stock')
 # parser.add_option('-x',         dest='x',      default=0,     action='storre',     type='int',  help='Index to grab x value')
 
@@ -62,51 +63,53 @@ print('ok')
 # encodedQuery = '%24CVEO'
 encodedQuery = '%24'+opts.sname
 
-request, response = Request(
-    'https://api.twitter.com/1.1/search/tweets.json?q=' + encodedQuery,
-    'Gdwmom36VPbk9p7XODaHf1qs7',
-    '4cZ8I713Du6LIsPHnt9LCelfU8rAnFURyII1XOXlgzXw1cdOTr')
-# print(request)
-# print(response)
+while True:
+    time.sleep(60)
+    request, response = Request(
+        'https://api.twitter.com/1.1/search/tweets.json?q=' + encodedQuery,
+        'Gdwmom36VPbk9p7XODaHf1qs7',
+        '4cZ8I713Du6LIsPHnt9LCelfU8rAnFURyII1XOXlgzXw1cdOTr')
+    # print(request)
+    # print(response)
 
-# decode with utf8 is needed for Python 3
-data = json.loads(response.decode('utf8'))
+    # decode with utf8 is needed for Python 3
+    data = json.loads(response.decode('utf8'))
 
-tweets = data['statuses']
-tweets2sq = []
-for d in reversed(tweets):
-    # print(d.keys())
-    print('text --->',d['text'])
-    print('created_at --->',d['created_at'])
-    print('user/screen_name --->', d['user']['screen_name'])
-    print('user/friends_count',    d['user']['friends_count'])
-    print('user/followers_count',  d['user']['followers_count'])
-    print('user/id_str',           d['user']['id_str'])
-    print('user/favourites_count', d['user']['favourites_count'])
-    # for k in d.keys():
-    #     if (k == 'text'):
-    #         print(k, '--->' , d[k].replace('\n', ' '))
-    tweets2sq.append((opts.sname, d['created_at'], d['user']['screen_name'], int(d['user']['friends_count']), int(d['user']['followers_count']), int(d['user']['id_str']), int(d['user']['favourites_count']), d['text'].replace('\n', ' ')))
-    print('\n')
+    tweets = data['statuses']
+    tweets2sq = []
+    for d in reversed(tweets):
+        # print(d.keys())
+        print('text --->',d['text'])
+        print('created_at --->',d['created_at'])
+        print('user/screen_name --->', d['user']['screen_name'])
+        print('user/friends_count',    d['user']['friends_count'])
+        print('user/followers_count',  d['user']['followers_count'])
+        print('user/id_str',           d['user']['id_str'])
+        print('user/favourites_count', d['user']['favourites_count'])
+        # for k in d.keys():
+        #     if (k == 'text'):
+        #         print(k, '--->' , d[k].replace('\n', ' '))
+        tweets2sq.append((opts.sname, d['created_at'], d['user']['screen_name'], int(d['user']['friends_count']), int(d['user']['followers_count']), int(d['user']['id_str']), int(d['user']['favourites_count']), d['text'].replace('\n', ' ')))
+        print('\n')
 
-print(encodedQuery)
-print(tweets[0].keys())
-pprint.pprint(tweets2sq)
-# Test changes.
+    print(encodedQuery)
+    print(tweets[0].keys())
+    pprint.pprint(tweets2sq)
+    # Test changes.
 
-# Project 1 will attempt to use yahoo-finance to retrieve stock data. These data will be stored alone with twitter data.
-# Testing code will be written in yahooFinTest.py
+    # Project 1 will attempt to use yahoo-finance to retrieve stock data. These data will be stored alone with twitter data.
+    # Testing code will be written in yahooFinTest.py
 
-# Project 2 Will attempt to use https://docs.python.org/2/library/sqlite3.html for database storage.
-# Test sqlite3
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
-# Create Table tw is not exist
-# This table is designed to store twitter info on stock symbols.
-# The table is also designed to be cumulative, so future data fishing is possible.
-# Table format
-c.execute('''CREATE TABLE IF NOT EXISTS tw (symbol text, created_at text, screen_name text, friends_count integer, followers_count integer, id_str integer, favourites_count integer, text text)''' )
-c.executemany('INSERT INTO tw VALUES (?,?,?,?,?,?,?,?)', tweets2sq)
-conn.commit()
-conn.close()
+    # Project 2 Will attempt to use https://docs.python.org/2/library/sqlite3.html for database storage.
+    # Test sqlite3
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    # Create Table tw is not exist
+    # This table is designed to store twitter info on stock symbols.
+    # The table is also designed to be cumulative, so future data fishing is possible.
+    # Table format
+    c.execute('''CREATE TABLE IF NOT EXISTS tw (symbol text, created_at text, screen_name text, friends_count integer, followers_count integer, id_str integer, favourites_count integer, text text)''' )
+    c.executemany('INSERT OR IGNORE INTO tw VALUES (?,?,?,?,?,?,?,?)', tweets2sq)
+    conn.commit()
+    conn.close()
 
